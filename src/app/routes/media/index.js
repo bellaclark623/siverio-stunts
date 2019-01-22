284a4f2d4525d266d4cce009795d7cada76761e6
Bella Clark <bellaclark623@gmail.com>
2019-01-21 23:40:22 -0500

    added year sorting to portfolio list

--- src/app/routes/media/index.js
+++ src/app/routes/media/index.js
@@ -1,22 +1,8 @@
 import React from "react";
-import filmsData from "../../data/films.json";
 import InteriorLayout from "../../layouts/InteriorLayout";
 
 export default () => (
-  <InteriorLayout
-    id="films"
-    title="Films"
-    description="View Manny Siverio's stunt work portfolio."
-  >
-    <div className="lists-wrapper">
-      {filmsData.map(film => {
-        return (
-          <div>
-            {film.name}
-            {film.captions}
-          </div>
-        );
-      })}
-    </div>
+  <InteriorLayout id="media" title="Media" description="TODO: write this">
+    <p>What we're all about</p>
   </InteriorLayout>
 );
