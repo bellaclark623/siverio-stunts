f47c65be6c12803e04ea5dd57127cdf3dde2aacb
Bella Clark <bellaclark623@gmail.com>
2019-01-26 19:42:20 -0500

    Added "Back to Top" button to Interior pages

--- src/app/layouts/InteriorLayout.js
+++ src/app/layouts/InteriorLayout.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Page from "../components/page";
 
-export default ({ id, children, title, description }) => (
-  <Page id={id} title={title} description={description}>
-    <div className="InteriorLayout-subheading">
-      <div className="container py-4">
-        <div className="row">
-          <div className="col-12">
-            <h3>{title}</h3>
-            <p>{description}</p>
+export default ({ id, children, title, description }) => {
+  const pageBody = children;
+
+  return (
+    <Page id={id} title={title} description={description}>
+      <div className="InteriorLayout-subheading" id="top">
+        <div className="container py-4">
+          <div className="row">
+            <div className="col-12">
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container pt-4 pb-5">{children}</div>
-  </Page>
-);
+      <div className="container pt-4 pb-5">{pageBody}</div>
+      <a href="#top" id="back-to-top">
+        Back to Top
+      </a>
+    </Page>
+  );
+};
