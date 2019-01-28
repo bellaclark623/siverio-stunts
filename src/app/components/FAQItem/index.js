215644c311273332b7482708d397e772ae4f8d6a
Bella Clark <bellaclark623@gmail.com>
2019-01-27 21:24:51 -0500

    FAQItems now toggle on click

--- src/app/components/FAQItem/index.js
+++ src/app/components/FAQItem/index.js
@@ -1,12 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
-export default ({ question, answer }) => (
-  <Fragment>
-    <dt>
-      <button className="faq-button">{question}</button>
-    </dt>
-    <dd>
-      <p>{answer}</p>
-    </dd>
-  </Fragment>
-);
+export default class FAQItem extends Component {
+  state = { collapsed: true };
+
+  handleButtonOnClick = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  render() {
+    const { question, answer } = this.props;
+
+    return (
+      <Fragment>
+        <dt>
+          <button onClick={this.handleButtonOnClick} className="faq-button">
+            {question}
+          </button>
+        </dt>
+        {!this.state.collapsed && (
+          <dd>
+            <p>{answer}</p>
+          </dd>
+        )}
+      </Fragment>
+    );
+  }
+}
